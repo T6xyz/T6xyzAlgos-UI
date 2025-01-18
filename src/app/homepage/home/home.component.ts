b3bad88062e96082e593e2457fc8993fdf891e38
T6xyz <benjaminzwolenik@gmail.com>
2025-01-18 18:22:25 -0500

    Added scrolling animations to homepage and cursor effects

--- src/app/homepage/home/home.component.ts
+++ src/app/homepage/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
+import { AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,27 @@ import {MatButtonModule} from '@angular/material/button';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+
+export class HomeComponent implements AfterViewInit {
   path: string = "/assets/T6xyzLogo.png";
   path1: string = "assets/Example-Lecture2.png";
   pathInstagram: string = "assets/instagramLogo.png";
   pathLinkedin: string = "assets/linkedinLogo.png";
-  pathYoutube: string = "assets/youtubeLogo.png"
+  pathYoutube: string = "assets/youtubeLogo.png";
+
+  ngAfterViewInit(): void {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add('show');
+        } else {
+          entry.target.classList.remove('show');
+        }
+      })
+    })
+    
+    const hiddenElements = document.querySelectorAll('.hidden');
+    hiddenElements.forEach((el) => observer.observe(el));  
+  }
 }
+
