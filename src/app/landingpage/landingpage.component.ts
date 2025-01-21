24b4f33153b8b4f67167fabf4ffc214ebfa099fe
T6xyz <benjaminzwolenik@gmail.com>
2025-01-21 15:00:44 -0500

    Added algorithms dropdown and fixed margin sizes

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -16,18 +16,60 @@ export class LandingpageComponent implements AfterViewInit{
         console.log("Out Worked!")
         document.getElementById("ds0")?.classList.remove("show0")
         document.getElementById("ds1")?.classList.remove("show1")
+        document.getElementById("a0")?.classList.remove("show0")
+        document.getElementById("a1")?.classList.remove("show1")
+        document.getElementById("dsid")?.classList.remove("showBar")
+        document.getElementById("aid")?.classList.remove("showBar")
       });
 
       document.getElementById("ds1")?.addEventListener("mouseleave", function() {
         console.log("Out Worked!")
         document.getElementById("ds0")?.classList.remove("show0")
         document.getElementById("ds1")?.classList.remove("show1")
+        document.getElementById("a0")?.classList.remove("show0")
+        document.getElementById("a1")?.classList.remove("show1")
+        document.getElementById("dsid")?.classList.remove("showBar")
+        document.getElementById("aid")?.classList.remove("showBar")
       });
 
       document.getElementById("dsid")?.addEventListener("mouseenter", function() {
         console.log("Hover Worked!")
         document.getElementById("ds0")?.classList.add("show0")
         document.getElementById("ds1")?.classList.add("show1")
+        document.getElementById("a0")?.classList.remove("show0")
+        document.getElementById("a1")?.classList.remove("show1")
+        document.getElementById("dsid")?.classList.add("showBar")
+        document.getElementById("aid")?.classList.remove("showBar")
+      });
+
+      document.getElementById("a0")?.addEventListener("mouseleave", function() {
+        console.log("Out Worked!")
+        document.getElementById("a0")?.classList.remove("show0")
+        document.getElementById("a1")?.classList.remove("show1")
+        document.getElementById("ds0")?.classList.remove("show0")
+        document.getElementById("ds1")?.classList.remove("show1")
+        document.getElementById("aid")?.classList.remove("showBar")
+        document.getElementById("dsid")?.classList.remove("showBar")
+      });
+
+      document.getElementById("a1")?.addEventListener("mouseleave", function() {
+        console.log("Out Worked!")
+        document.getElementById("a0")?.classList.remove("show0")
+        document.getElementById("a1")?.classList.remove("show1")
+        document.getElementById("ds0")?.classList.remove("show0")
+        document.getElementById("ds1")?.classList.remove("show1")
+        document.getElementById("aid")?.classList.remove("showBar")
+        document.getElementById("dsid")?.classList.remove("showBar")
+      });
+
+      document.getElementById("aid")?.addEventListener("mouseenter", function() {
+        console.log("Hover Worked!")
+        document.getElementById("a0")?.classList.add("show0")
+        document.getElementById("a1")?.classList.add("show1")
+        document.getElementById("ds0")?.classList.remove("show0")
+        document.getElementById("ds1")?.classList.remove("show1")
+        document.getElementById("aid")?.classList.add("showBar")
+        document.getElementById("dsid")?.classList.remove("showBar")
       });
   }
 }
