f3ad7bedf2b7aa4ad16f0fca209b53b1060edad7
T6xyz <benjaminzwolenik@gmail.com>
2025-02-09 03:19:32 -0500

    Added new landing page design for Data Structures UI

--- src/app/landingpage/landingpage.component.ts
+++ src/app/landingpage/landingpage.component.ts
@@ -1,75 +1,30 @@
 import { Component } from '@angular/core';
 import { AfterViewInit } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-landingpage',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './landingpage.component.html',
   styleUrl: './landingpage.component.css'
 })
 export class LandingpageComponent implements AfterViewInit{
   path: string = "/assets/T6xyzLogo.png";
+  pathDS: string = "assets/pageDS.mp4";
+  circumference: number = 439.6;
+  offset: number = 109.9;
 
   ngAfterViewInit(): void {
 
-      document.getElementById("ds0")?.addEventListener("mouseleave", function() {
-        console.log("Out Worked!")
-        document.getElementById("ds0")?.classList.remove("show0")
-        document.getElementById("ds1")?.classList.remove("show1")
-        document.getElementById("a0")?.classList.remove("show0")
-        document.getElementById("a1")?.classList.remove("show1")
-        document.getElementById("dsid")?.classList.remove("showBar")
-        document.getElementById("aid")?.classList.remove("showBar")
-      });
+  }
+  playVideo() {
+    let video = <HTMLVideoElement> document.getElementById("dsCardVid")
+    video?.play()
+  }
 
-      document.getElementById("ds1")?.addEventListener("mouseleave", function() {
-        console.log("Out Worked!")
-        document.getElementById("ds0")?.classList.remove("show0")
-        document.getElementById("ds1")?.classList.remove("show1")
-        document.getElementById("a0")?.classList.remove("show0")
-        document.getElementById("a1")?.classList.remove("show1")
-        document.getElementById("dsid")?.classList.remove("showBar")
-        document.getElementById("aid")?.classList.remove("showBar")
-      });
-
-      document.getElementById("dsid")?.addEventListener("mouseenter", function() {
-        console.log("Hover Worked!")
-        document.getElementById("ds0")?.classList.add("show0")
-        document.getElementById("ds1")?.classList.add("show1")
-        document.getElementById("a0")?.classList.remove("show0")
-        document.getElementById("a1")?.classList.remove("show1")
-        document.getElementById("dsid")?.classList.add("showBar")
-        document.getElementById("aid")?.classList.remove("showBar")
-      });
-
-      document.getElementById("a0")?.addEventListener("mouseleave", function() {
-        console.log("Out Worked!")
-        document.getElementById("a0")?.classList.remove("show0")
-        document.getElementById("a1")?.classList.remove("show1")
-        document.getElementById("ds0")?.classList.remove("show0")
-        document.getElementById("ds1")?.classList.remove("show1")
-        document.getElementById("aid")?.classList.remove("showBar")
-        document.getElementById("dsid")?.classList.remove("showBar")
-      });
-
-      document.getElementById("a1")?.addEventListener("mouseleave", function() {
-        console.log("Out Worked!")
-        document.getElementById("a0")?.classList.remove("show0")
-        document.getElementById("a1")?.classList.remove("show1")
-        document.getElementById("ds0")?.classList.remove("show0")
-        document.getElementById("ds1")?.classList.remove("show1")
-        document.getElementById("aid")?.classList.remove("showBar")
-        document.getElementById("dsid")?.classList.remove("showBar")
-      });
-
-      document.getElementById("aid")?.addEventListener("mouseenter", function() {
-        console.log("Hover Worked!")
-        document.getElementById("a0")?.classList.add("show0")
-        document.getElementById("a1")?.classList.add("show1")
-        document.getElementById("ds0")?.classList.remove("show0")
-        document.getElementById("ds1")?.classList.remove("show1")
-        document.getElementById("aid")?.classList.add("showBar")
-        document.getElementById("dsid")?.classList.remove("showBar")
-      });
+  pauseVideo() {
+    let video = <HTMLVideoElement> document.getElementById("dsCardVid")
+    video?.pause();
+    video.currentTime = 0;
   }
 }
