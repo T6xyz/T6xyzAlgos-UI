53da3153c6de22f9f606518103c0699b9a998785
T6xyz <benjaminzwolenik@gmail.com>
2025-03-08 01:47:45 -0500

    Moved the home component styles to the global styles file

--- src/app/homepage/home/home.component.ts
+++ src/app/homepage/home/home.component.ts
@@ -2,20 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatButtonModule} from '@angular/material/button';
 import { AfterViewInit } from '@angular/core';
+import { InfobarComponent } from '../../infobar/infobar.component';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterOutlet, MatButtonModule],
+  imports: [RouterOutlet, MatButtonModule, InfobarComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 
 export class HomeComponent implements AfterViewInit {
   path: string = "/assets/T6xyzLogo.png";
-  path1: string = "assets/Example-Lecture2.png";
-  pathInstagram: string = "assets/instagramLogo.png";
-  pathLinkedin: string = "assets/linkedinLogo.png";
-  pathYoutube: string = "assets/youtubeLogo.png";
   pathPage1: string = "assets/homepage1.mp4";
   pathPage2: string = "assets/homepage2.mp4";
   pathPage3: string = "assets/homepage3.mp4";
@@ -25,14 +22,14 @@ export class HomeComponent implements AfterViewInit {
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          entry.target.classList.add('show');
+          entry.target.classList.add('showHome');
         } else {
-          entry.target.classList.remove('show');
+          entry.target.classList.remove('showHome');
         }
       })
     })
     
-    const hiddenElements = document.querySelectorAll('.hidden');
+    const hiddenElements = document.querySelectorAll('.hiddenHome');
     hiddenElements.forEach((el) => observer.observe(el));  
   }
 }
