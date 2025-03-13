123e3a0fcbf4f60f367bd898d63e94847060dccc
T6xyz <benjaminzwolenik@gmail.com>
2025-03-13 17:56:03 -0400

    Created register page and added new input data for registration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { OnInit, AfterViewInit } from '@angular/core';
+import { InfobarComponent } from '../infobar/infobar.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [InfobarComponent, MatCheckboxModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements AfterViewInit, OnInit {
+  pathLoginVid: string = "assets/pageLogin.mp4"
+  backIcon: string = "assets/backIcon.png"
 
+  ngOnInit(): void {
+      
+  }
+  ngAfterViewInit(): void {
+      
+  }
 }
