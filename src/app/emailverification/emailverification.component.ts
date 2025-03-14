05a4940f7aa7fbd63f48b672efd17c348fb29d0c
T6xyz <benjaminzwolenik@gmail.com>
2025-03-14 11:43:32 -0400

    added verification, error, forgot password, and settings components

---
+++ src/app/emailverification/emailverification.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-emailverification',
+  imports: [],
+  templateUrl: './emailverification.component.html',
+  styleUrl: './emailverification.component.scss'
+})
+export class EmailverificationComponent {
+
+}
